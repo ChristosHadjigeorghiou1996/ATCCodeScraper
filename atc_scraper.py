7aba81c9d607739504cae0ca15beaaebd075ea16
Christos <christoshadjigeorghiou@gmail.com>
2025-08-05 21:26:23 +0300

    Add atc subcategory parsing

--- atc_scraper.py
+++ atc_scraper.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-from re import compile
+from re import compile, Pattern
+from time import sleep
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from requests import get
 from tabulate import tabulate
 
@@ -12,7 +13,7 @@ class ATCScraper:
     """
     Class to scraper atc codes from the website and parse them to ATCCode
     """
-    def __init__(self, base_url: str):
+    def __init__(self, base_url: str = "https://atcddd.fhi.no/"):
         self.base_url = base_url
         self.atc_index = f"{self.base_url}/atc_ddd_index/"
         self.headers = {
@@ -20,40 +21,81 @@ class ATCScraper:
         }
         self.timeout = 10
         self.code_pattern = compile(r"code=([A-Z])")
+        self.subcategory_pattern = compile(r"code=([A-Z0-9]+)")
         self.soup = None
+        # remember visited code to avoid in recursion
+        self.visited: set[str] = set()
 
-    def parse_website_with_soup(self) -> BeautifulSoup:
+    def parse_website_with_soup(self, website: str) -> BeautifulSoup:
         """
         Parse the website to bs4 object to parse it.
         """
-        response = get(self.atc_index, headers=self.headers, timeout=10)
-        self.soup = BeautifulSoup(response.content, "html.parser")
+        sleep(.5)
+        response = get(website, headers=self.headers, timeout=10)
+        soup = BeautifulSoup(response.content, "html.parser")
+        return soup
 
-    def obtain_atc_categories(self) -> list[ATCCode]:
-        # find the title and get the next paragraph containing the categories
-        atc_h3 = self.soup.find("h3", string="ATC code")
-        atc_paragraph = atc_h3.find_next("p")
-        # parse the 
+    def _extract_atc_codes_from_text(self, content_to_parse: Tag, regex_pattern: Pattern) -> list[ATCCode]:
+        """
+        Parse the content of the ATC page and get the codes according to regex
+        :param content_to_parse: tag element of paragraph to parse
+        :param regex_pattern: pattern to apply to get code
+        :return list of ATCCode
+        """
         atc_categories: list[ATCCode] = []
-        for a in atc_paragraph.find_all("a"):
+        for a in content_to_parse.find_all("a"):
             href: str = a.get("href", "")
-            code_match = self.code_pattern.search(href)
+            code_match = regex_pattern.search(href)
             if code_match:
                 code = code_match.group(1)
                 name = a.text.strip()
-                atc_code = ATCCode(code, name, f"{self.atc_index}{href.lstrip('./')}")
+                url = f"{self.atc_index}{href.lstrip('./')}"
+                atc_code = ATCCode(code, name, url)
                 atc_categories.append(atc_code)
         return atc_categories
 
+    def obtain_atc_categories(self) -> list[ATCCode]:
+        """
+        Get the main categories of ATC
+        :return the list of extracted ATCCode
+        """
+        website_soup = self.parse_website_with_soup(self.atc_index)
+        sleep(.5)
+        # find the title and get the next paragraph containing the categories
+        atc_h3 = website_soup.find("h3", string="ATC code")
+        atc_paragraph = atc_h3.find_next("p")
+        return self._extract_atc_codes_from_text(atc_paragraph, self.code_pattern)
+
+    def obtain_atc_subcategories(self, atc_category: ATCCode) -> list[ATCCode]:
+        if atc_category.url in self.visited:
+            return []
+        self.visited.add(atc_category.url)
+        atc_subcategories: list[ATCCode] = []
+        c_website_soup = self.parse_website_with_soup(atc_category.url)
+        atc_c_code = c_website_soup.find(string=atc_category.name)
+        if not atc_c_code:
+            return []
+        atc_subcategory_paragraph = atc_c_code.find_next("p")
+        if not atc_subcategory_paragraph:
+            return []
+        atc_subcategories = self._extract_atc_codes_from_text(atc_subcategory_paragraph, self.subcategory_pattern)
+        for subcategory in atc_subcategories:
+            atc_subcategories.extend(self.obtain_atc_subcategories(subcategory))
+        return atc_subcategories
 
 
 if __name__ == "__main__":
-    atc_scraper = ATCScraper("https://atcddd.fhi.no/")
-    atc_scraper.parse_website_with_soup()
-    atc_categories = atc_scraper.obtain_atc_categories()
+    atc_scraper = ATCScraper()
+    extracted_atc_categories: list[ATCCode] = list()
     filepath = Path("atc_categories.csv")
-    FileHelper.save_to_csv(filepath, atc_categories)
-    if filepath.exists():
+    if not filepath.exists():
+        atc_categories = atc_scraper.obtain_atc_categories()
+        extracted_atc_categories.extend(atc_categories)
+        for atc_category in atc_categories:
+            print(atc_category)
+            atc_subcategories = atc_scraper.obtain_atc_subcategories(atc_category)
+            extracted_atc_categories.extend(atc_subcategories)
+        FileHelper.save_to_csv(filepath, extracted_atc_categories)
+    else:
         rows = FileHelper.read_from_csv(filepath)
         print(tabulate(rows[1:], headers=rows[0], tablefmt="grid"))
-
---
+++ releasenotes.md
@@ -0,0 +1,7 @@
+# Version 1.0.0
+Release Date: TBD
+
+<ul>
+<li> <a href="https://github.com/ChristosHadjigeorghiou1996/ATCCodeScraper/issues/1">1</a> - Add atc subcategory parsing
+</li>
+</ul>
